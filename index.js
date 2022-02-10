3f4219da7bf81458533b56594c6535c67ea678f7
Reid Schroder <reidschroder2@gmail.com>
2022-02-10 00:52:00 -0500

    added initial questions

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address (Required)',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your email address');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+              } else {
+                console.log('Please enter the title of your project');
+                return false;
+              }
+            }
+        },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license you would like to add to this project',
+        choices: [
+            'MIT',
+            'ISC',
+            'APACHE',
+            'GNU'
+        ]
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
